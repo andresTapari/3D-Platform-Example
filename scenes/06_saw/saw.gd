e840a43e7e71f3dba9dfdf2eb8de8381867bad0c
andresTapa <79794569+andresTapari@users.noreply.github.com>
2024-05-02 19:55:26 -0300

    sky sprite changed

--- scenes/06_saw/saw.gd
+++ scenes/06_saw/saw.gd
@@ -1,4 +1,5 @@
 extends MovilPlatform
+
 signal game_over
 
 func _physics_process(delta):
@@ -9,6 +10,8 @@ func _physics_process(delta):
 
 # Se ejecuta cuando un cuerpo entra en el área de detección
 func _on_area_3d_body_entered(body):
+	# Si body es del grupo player
 	if body.is_in_group("player"):
+		# emite evento de game over
 		game_over.emit()
 
