d41119a6d1422c545c31afb67fb78e0f4dcf95a3
andresTapa <79794569+andresTapari@users.noreply.github.com>
2024-05-03 13:09:20 -0300

    minnor fixs

--- scenes/07_interactive/activePlatform/activePlatform.gd
+++ scenes/07_interactive/activePlatform/activePlatform.gd
@@ -2,13 +2,13 @@ extends MovilPlatform
 
 @export var triggerNode: NodePath = ""
 
-
 func _ready():
 	super()
 	if not triggerNode.is_empty():
 		get_node(triggerNode).trigger.connect(_handle_trigger_event)
+	isEnable = false
 
 ## 
-func _handle_trigger_event(turn_state) -> void:
+func _handle_trigger_event(turn_state: bool) -> void:
 	isEnable = turn_state
 
